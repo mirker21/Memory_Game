42951e2666050836c13bb3c3d3bc2100e336e0c4
mirker21 <95049388+mirker21@users.noreply.github.com>
2024-08-06 12:38:31 -0600

    Preset.jsx - replaced '.' with '../../../dist'
    fixed the file path

--- src/display/custom_tile_layout/Preset.jsx
+++ src/display/custom_tile_layout/Preset.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { GameSettingsContext } from "../../Game";
 
 const imagePresetFiles = import.meta.glob(
-    '/public/presets_library/layout_presets/*.png'
+    '../../../dist/presets_library/layout_presets/*.png'
 );
 let imagePresetFileNames = Object.keys(imagePresetFiles)
 
@@ -58,7 +58,7 @@ export default function Preset({presetLayouts, setPresetLayouts, layoutCount}) {
                                 className="custom-single-file-container" 
                                 title={
                                     fileName
-                                    .replace('/public/presets_library/layout_presets/', '')
+                                    .replace('../../../dist/presets_library/layout_presets/', '')
                                     .replace('.png', '')
                                     .replace(/_/g, ' ')
                                 }
@@ -79,7 +79,7 @@ export default function Preset({presetLayouts, setPresetLayouts, layoutCount}) {
                                 >
                                     {
                                         fileName
-                                        .replace('/public/presets_library/layout_presets/', '')
+                                        .replace('../../../dist/presets_library/layout_presets/', '')
                                         .replace('.png', '')
                                         .replace(/_/g, ' ')
                                     }
