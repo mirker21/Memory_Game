457d3c0c2f98c55c2d9c998c30eda726a5010a88
mirker21 <95049388+mirker21@users.noreply.github.com>
2024-08-07 10:19:22 -0600

    Fixed GET_PRESETS function App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Game from "./Game"
 import fs from 'fs';
 import path from 'path';
 
-export function GET(request) {
-  let usersPath = path.join(process.cwd(), 'users.json');
-  let file = fs.readFileSync(usersPath);
-  return new Response(file);
+export function GET_PRESETS(subdirectory) {
+  let presetsPath = path.join(process.cwd(), '/presets_library/' + subdirectory);
+  let files = fs.readdirSync(presetsPath);
+  return new Response(files);
 }
 
 function App() {
