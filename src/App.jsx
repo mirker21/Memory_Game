43ac7a613807bb34941b732e203e959dba838435
mirker21 <95049388+mirker21@users.noreply.github.com>
2024-08-07 10:07:48 -0600

    Added GET function App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import Game from "./Game"
 
+import fs from 'fs';
+import path from 'path';
+
+export function GET(request) {
+  let usersPath = path.join(process.cwd(), 'users.json');
+  let file = fs.readFileSync(usersPath);
+  return new Response(file);
+}
+
 function App() {
 
   return (
